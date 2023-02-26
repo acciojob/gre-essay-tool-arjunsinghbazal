69a41311b7af7e05b84dd89b0bd03bfd88fb0396
Arjun Singh <55157623+arjunsinghbazal@users.noreply.github.com>
2023-02-26 23:06:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your code here
+function countWords() {
+			let text = document.getElementById("evaluatedText").value;
+			let wordCount = 0;
+			if (text.trim() !== "") {
+				// regx '\s+' will split at one or more spaces
+				// trim() will remove trailing and leading spaces
+				wordCount = text.trim().split(/\s+/).length;
+			}
+			document.getElementById("wordCount").textContent = wordCount;
+		}
